8cc3f880c498968451a2b6195e1e1801673ca30e
kez <kerrialbeckettnewham@gmail.com>
2024-03-25 16:21:40 +0100

    add random quote

--- src/Controller/Controller/AppController.php
+++ src/Controller/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Controller;
 
+use App\Model\Quote;
 use App\Repository\ArticleRepository;
 use App\Repository\VentureRepository;
 use Knp\Component\Pager\Pagination\PaginationInterface;
@@ -14,8 +15,8 @@ class AppController extends AbstractController
 {
 
     public function __construct(
-        private readonly ArticleRepository $articleRepository,
-        private readonly VentureRepository $ventureRepository,
+        private readonly ArticleRepository  $articleRepository,
+        private readonly VentureRepository  $ventureRepository,
         private readonly PaginatorInterface $paginator
     )
     {
@@ -25,7 +26,21 @@ class AppController extends AbstractController
     public function index(): Response
     {
         $articles = $this->articleRepository->findRecentlyPublished();
+
+        $quotes = [
+            new Quote(quote: 'That\'s all any of us are: amateurs. we don\'t live long enough to be anything else.', author: 'Charlie Chaplin'),
+            new Quote(quote: 'Pull back the curtain on your process', author: 'Ann Friedman'),
+            new Quote(quote: 'This is the true joy in life, being used for a purpose recognized by yourself as a mighty one. Being a force of nature instead of a feverish, selfish little clod of ailments and grievances, complaining that the world will not devote itself to making you happy. I am of the opinion that my life belongs to the whole community and as long as I live, it is my privilege to do for it what I can. I want to be thoroughly used up when I die, for the harder I work, the more I live. I rejoice in life for its own sake. Life is no brief candle to me. It is a sort of splendid torch which I have got hold of for the moment and I want to make it burn as brightly as possible before handing it on to future generations.', author: 'George Bernard Shaw'),
+            new Quote(quote: 'Wealth of information creates a poverty of attention', author: 'Herbert Simon'),
+            new Quote(quote: 'Avoid having your ego so close to your position that when your position falls, your ego goes with it.', author: 'Colin Powell'),
+            new Quote(quote: 'There is no enjoying the possession of anything valuable unless one has someone to share it with', author: 'Seneca'),
+            new Quote(quote: 'Success consists of going from failure to failure without loss of enthusiasm', author: 'Winston Churchill'),
+        ];
+
+        $quote = $quotes[rand(0, count($quotes) - 1)];
+
         return $this->render('app/ladning.html.twig', [
+            'quote' => $quote,
             'articles' => $articles
         ]);
     }
---
+++ src/Model/Quote.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model;
+
+final readonly class Quote
+{
+
+    private string $author;
+    private string $quote;
+
+    /**
+     * @param string $author
+     * @param string $quote
+     */
+    public function __construct(string $quote, string $author)
+    {
+        $this->quote = $quote;
+        $this->author = $author;
+    }
+
+    public function getAuthor(): string
+    {
+        return $this->author;
+    }
+
+    public function getQuote(): string
+    {
+        return $this->quote;
+    }
+
+}
