a87ad844cb180a1e4751fb602ff680b31e77143f
kez <kerrialbeckettnewham@gmail.com>
2024-03-25 14:02:30 +0100

    update app entry point

--- importmap.php
+++ importmap.php
@@ -13,14 +13,14 @@
  */
 return [
     'app' => [
-        'path' => './assets/app.js',
+        'path' => 'app.js',
         'entrypoint' => true,
     ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
     '@symfony/stimulus-bundle' => [
-        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+        'path' => '@symfony/stimulus-bundle/loader.js',
     ],
     '@hotwired/turbo' => [
         'version' => '7.3.0',
