3b8bbbf9cea895ce49bbfcad1a98a2151f25aa5a
kez <kerrialbeckettnewham@gmail.com>
2024-05-06 15:47:18 +0200

    update asset compile

---
+++ public/assets/@symfony/stimulus-bundle/controllers-407116ba1c9ba4177ac220cda6e18858.js
@@ -0,0 +1,9 @@
+import controller_0 from "../ux-autocomplete/controller.js";
+import "tom-select/dist/css/tom-select.bootstrap5.css";
+import controller_1 from "../ux-live-component/live_controller.js";
+import "../ux-live-component/live.min.css";
+import controller_2 from "../ux-turbo/turbo_controller.js";
+import controller_3 from "../../controllers/hello_controller.js";
+export const eagerControllers = {"symfony--ux-autocomplete--autocomplete": controller_0, "live": controller_1, "symfony--ux-turbo--turbo-core": controller_2, "hello": controller_3};
+export const lazyControllers = {};
+export const isApplicationDebug = true;
